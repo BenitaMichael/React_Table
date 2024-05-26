17c9d198e1c16f034ebbe95bd8e323f2fd8c7e78
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2024-05-26 13:53:07 +0100

    feat: setup files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+
+// COMPONENTS
+import AppNavbar from './Components/AppNavbar';
+import AppFooter from './Components/AppFooter';
+
+
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+    </>
+  );
+}
+
+export default App;
