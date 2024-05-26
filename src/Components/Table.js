dcc08862d42318102f0d2102ad15cb91149f8691
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2024-05-26 15:27:44 +0100

    feat: Search functionality added

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Table = ({ columns, data }) => {
+  const [searchItem, setSearchItem] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchItem(event.target.value);
+  };
+
+  const filteredItem = data.filter((row) =>
+    columns.some(
+      (column) =>
+        row[column.accessor]
+          .toString()
+          .toLowerCase()
+          .indexOf(searchItem.toLowerCase()) > -1
+    )
+  );
+
   return (
     <div className='table-container'>
-        <table>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchItem}
+        onChange={handleSearch}
+        style={{ padding: '8px', width: '50%'}}
+      />
+      <table>
         <thead>
-            <tr>
+          <tr>
             {columns.map((column) => (
-                <th key={column.accessor}>{column.header}</th>
+              <th key={column.accessor}>{column.header}</th>
             ))}
-            </tr>
+          </tr>
         </thead>
         <tbody>
-            {data.map((row) => (
+          {filteredItem.map((row) => (
             <tr key={row.plugID}>
-                {columns.map((column) => (
+              {columns.map((column) => (
                 <td key={column.accessor}>{row[column.accessor]}</td>
-                ))}
+              ))}
             </tr>
-            ))}
+          ))}
         </tbody>
-        </table>
+      </table>
     </div>
   );
 };
