f14dbd013f1599f9dedbab574fe321c85a354be6
BenitaMichael <107327075+BenitaMichael@users.noreply.github.com>
2024-05-26 14:49:02 +0100

    feat: Table definition

--- src/App.js
+++ src/App.js
@@ -4,11 +4,67 @@ import './App.css';
 // COMPONENTS
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Table from './Components/Table'
 
 function App() {
+  const data = [
+    {
+      "plugID": "gdcnye7",
+      "plugin_name": "SportyBet",
+      "plugin_location": "Nigeria",
+      "plugin_stats": "active",
+      "plugin_rate": "40%"
+    },
+    {
+      "plugID": "wusolk2",
+      "plugin_name": "Flashscore",
+      "plugin_location": "Austraila",
+      "plugin_stats": "disabled",
+      "plugin_rate": "20%"
+    },
+    {
+      "plugID": "hgvuje7",
+      "plugin_name": "Betway",
+      "plugin_location": "Nigeria",
+      "plugin_stats": "active",
+      "plugin_rate": "65%"
+    },
+    {
+      "plugID": "nvfdks6",
+      "plugin_name": "Betfair",
+      "plugin_location": "USA",
+      "plugin_stats": "active",
+      "plugin_rate": "30%"
+    },
+    {
+      "plugID": "lxjsns4",
+      "plugin_name": "Hollybets",
+      "plugin_location": "South Africa",
+      "plugin_stats": "disabled",
+      "plugin_rate": "50%"
+    },
+    {
+      "plugID": "ydjnxn0",
+      "plugin_name": "Bet9ja",
+      "plugin_location": "Nigeria",
+      "plugin_stats": "disabled",
+      "plugin_rate": "40%"
+    }
+  ];
+
+  const columns = [
+    { header: 'Plug ID', accessor: 'plugID' },
+    { header: 'Plugin Name', accessor: 'plugin_name' },
+    { header: 'Location', accessor: 'plugin_location' },
+    { header: 'Status', accessor: 'plugin_stats' },
+    { header: 'Rate', accessor: 'plugin_rate' },
+  ];
+
+
   return (
     <>
       <Header />
+      <Table columns={columns} data={data} />
       <Footer />
     </>
   );
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -2,24 +2,26 @@ import React from 'react';
 
 const Table = ({ columns, data }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((column) => (
-            <th key={column.accessor}>{column.header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row) => (
-          <tr key={row.plugID}>
+    <div className='table-container'>
+        <table>
+        <thead>
+            <tr>
             {columns.map((column) => (
-              <td key={column.accessor}>{row[column.accessor]}</td>
+                <th key={column.accessor}>{column.header}</th>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+            </tr>
+        </thead>
+        <tbody>
+            {data.map((row) => (
+            <tr key={row.plugID}>
+                {columns.map((column) => (
+                <td key={column.accessor}>{row[column.accessor]}</td>
+                ))}
+            </tr>
+            ))}
+        </tbody>
+        </table>
+    </div>
   );
 };
 
